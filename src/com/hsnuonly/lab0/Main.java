484205b469d401d59b3d4ae04394fa7cd3f0c56e
Tsung-Hsin Wu <Tsung-Hsin Wu>
2017-02-23 16:54:03 +0800

    Initial commit

---
+++ src/com/hsnuonly/lab0/Main.java
@@ -0,0 +1,10 @@
+package com.hsnuonly.lab0;
+
+/**
+ * Created by hsnuonly on 2017/2/23.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("104062104\n");
+    }
+}
